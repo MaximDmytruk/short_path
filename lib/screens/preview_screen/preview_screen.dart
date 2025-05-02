2d66ad5568976c30afe1f4443977309d919585aa
Maksym Dmytruk <dmitruk.max@gmail.com>
2025-05-03 01:51:32 +0300

    Add widgets in PreviewScreen

--- lib/screens/preview_screen/preview_screen.dart
+++ lib/screens/preview_screen/preview_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:short_path/constants/colors_app/colors_app.dart';
 import 'package:short_path/widgets/custom_app_bars/custom_app_bar.dart';
 
 class PreviewScreen extends StatefulWidget {
@@ -13,6 +14,34 @@ class PreviewScreen extends StatefulWidget {
 class _PreviewScreenState extends State<PreviewScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: CustomAppBar(titleName: 'Preview screen'));
+    return Scaffold(
+      appBar: CustomAppBar(titleName: 'Preview screen'),
+      body: Column(
+        children: [
+          GridView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: 16,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+            ),
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Colors.black),
+                ),
+                child: Center(
+                  child: Text('index $index', textAlign: TextAlign.center),
+                ),
+              );
+            },
+          ),
+          Text(
+            '(0,0) => (0,1) => (0,2)',
+            style: TextStyle(color: ColorsApp.textColor, fontSize: 20),
+          ),
+        ],
+      ),
+    );
   }
 }
